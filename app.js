f76aab3f9500608f6124554537b44b8110d5a758
Emily Crook <Emily.A.Crook96@gmail.com>
2021-11-15 11:18:12 -0600

    finished with models

--- app.js
+++ app.js
@@ -6,13 +6,8 @@ const app = express();
 const tasks = require('./routes/tasks');
 
 //middleware
+app.use(express.static('./public'));
 app.use(express.json());
-
-//routes
-app.get('/hello', (req, res) => {
-    res.send('Task Manager App')
-});
-
 app.use('/api/v1/tasks', tasks);
 
 const port = 3000;
