3099686660b840a0491808b304a213a528d74134
Emily Crook <Emily.A.Crook96@gmail.com>
2021-11-15 12:15:21 -0600

    refactored the error messages and async await funcitons for cleaner code

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const connectDB = require('./db/connect');
 require('dotenv').config();
-
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler')
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
@@ -8,7 +9,11 @@ const tasks = require('./routes/tasks');
 //middleware
 app.use(express.static('./public'));
 app.use(express.json());
+
+//routes
 app.use('/api/v1/tasks', tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const port = 3000;
 
